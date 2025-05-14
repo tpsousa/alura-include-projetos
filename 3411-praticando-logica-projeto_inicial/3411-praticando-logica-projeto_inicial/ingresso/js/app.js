5712dcc02da4be331d5812937db9c881c1d8a7b1
Thiago Pereira Sousa <JT0020362@MATEUS.DC>
2025-05-14 11:34:12 -0300

    projeto final

---
+++ 3411-praticando-logica-projeto_inicial/3411-praticando-logica-projeto_inicial/ingresso/js/app.js
@@ -0,0 +1,27 @@
+
+
+function add () {
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
+
+    amigo.value = '';
+
+    if(lista.textContent == ''){
+        lista.textContent = amigo.value
+    }else {
+        lista.textContent = lista.textContent + ' ' + amigo.value;
+    }
+
+}
+
+function sort() {
+
+}
+
+
+
+/* Criar uma funcionalidade no Projeto Amigo Secreto que permita ao usuário clicar em um amigo na lista e removê-lo. Atualizar a visualização da lista após a remoção.
+Adicione os números 1,2,3 em uma variável chamada minhaLista e armazene os números 4, 5 e 6 separadamente em outra variável. Use o método concat() para combinar as sequências de valores em uma nova lista chamadanovaLista. Imprima novaLista no console.
+Remova o último elemento de novaLista. Imprima novaLista após a remoção.
+Utilize o algoritmo de Fisher-Yates (também conhecido como Knuth Shuffle) para embaralhar os elementos em novaLista. Pesquise e adapte o código para realizar o embaralhamento.
+Crie uma função chamada removerDuplicatas que aceita um array como parâmetro e retorna um novo array sem elementos duplicados. Teste a função com novaLista e imprima o array resultante. */
