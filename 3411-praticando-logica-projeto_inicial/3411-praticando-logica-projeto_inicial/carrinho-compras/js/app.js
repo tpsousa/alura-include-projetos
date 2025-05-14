a84a34f8dbe4fe6280214a5106b2c5f6288e71a7
Thiago Pereira Sousa <JT0020362@MATEUS.DC>
2025-05-14 10:42:06 -0300

    new project at alura

--- 3411-praticando-logica-projeto_inicial/3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ 3411-praticando-logica-projeto_inicial/3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -1,2 +1,37 @@
 
 
+
+
+
+
+
+//function to add a item
+const add =()=> {
+    let produto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+    
+    let nome = produto.splice('-');
+
+    let valor = produto.splice('R$')[1];
+
+    
+
+    //let total_produtos = quantidade * valor;
+
+    alert(produto);
+
+    alert(quantidade);
+
+    alert(nome);
+
+    alert(valor);
+
+    //alert(total_produtos);
+}
+//function to clear the input
+
+const clear =()=>{
+  let produto = document.getElementById('produto').value = "";
+
+  let quantidade = document.getElementById('quantidade').value = "";
+}
